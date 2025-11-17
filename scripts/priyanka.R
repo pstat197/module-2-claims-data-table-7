fa011eec63115ad7f222e65f396bf2257d490b2b
priyankaadurai <priyankadurai@ucsb.edu>
2025-11-16 17:44:54 -0800

    Add code for preliminary tasks question 3

--- scripts/priyanka.R
+++ scripts/priyanka.R
@@ -1,5 +1,7 @@
 #####################-QUESTION 1-####################################################
 
+####-Add Headers-############
+
 ## this script contains functions for preprocessing
 ## claims data; intended to be sourced 
 require(tidyverse)
@@ -9,6 +11,8 @@ require(rvest)
 require(qdapRegex)
 require(stopwords)
 require(tokenizers)
+library(tidyr)
+library(dplyr)
 
 # function to parse html and clean text
 parse_fn <- function(.html){
@@ -50,7 +54,7 @@ parse_data <- function(.df){
     filter(str_detect(text_tmp, '<!')) %>%
     rowwise() %>%
     mutate(text_clean = parse_fn(text_tmp)) %>%
-    unnest(text_clean) 
+    unnest_wider(text_clean) 
   return(out)
 }
 
@@ -75,6 +79,144 @@ nlp_fn <- function(parse_data.out){
 }
 
 claims_parsed <- parse_data(claims_raw)
+claims_nlp <- nlp_fn(claims_parsed)
 
+######-Fit PCA Logistic Regression Model-##########
+library(dplyr)
+library(rsample)
+library(yardstick)
+library(broom)   # for augment()
+library(modelr)
 
+claims_subset <- claims_nlp %>%
+  select(-.id) %>%
+  mutate(class = ifelse(bclass == "Relevant claim content", 1, 0)) %>%
+  select(-bclass)
 
+set.seed(101422)
+claims_split <- initial_split(claims_subset, prop = 0.8)
+train_data <- training(claims_split)
+View(train_data)
+test_data  <- testing(claims_split)
+
+X_train <- as.matrix(select(train_data, -class))
+X_test  <- as.matrix(select(test_data, -class))
+
+zero_var_cols <- apply(X_train, 2, function(col) var(col) == 0)
+X_train_filtered <- X_train[, !zero_var_cols]
+X_test_filtered  <- X_test[, !zero_var_cols]
+
+pca_res <- prcomp(X_train_filtered, center = TRUE, scale. = FALSE)
+cumvar <- cumsum(pca_res$sdev^2 / sum(pca_res$sdev^2))
+n_pc <- which(cumvar >= 0.9)[1]
+
+X_train_pca <- as.data.frame(pca_res$x[, 1:n_pc])
+X_test_pca  <- as.data.frame(predict(pca_res, newdata = X_test_filtered)[, 1:n_pc])
+
+X_train_pca$class <- train_data$class
+X_test_pca$class  <- test_data$class
+
+fit_pca <- glm(class ~ ., data = X_train_pca, family = "binomial")
+
+test_preds <- augment(fit_pca, newdata = X_test_pca, type.predict = "response") %>%
+  rename(pred_prob = .fitted) %>%
+  mutate(
+    estimate = factor(ifelse(pred_prob > 0.5, 1, 0), levels = c(0,1)),
+    truth    = factor(class, levels = c(0,1))
+  )
+
+class_metrics <- metric_set(
+  yardstick::accuracy,
+  yardstick::sensitivity,
+  yardstick::specificity,
+  yardstick::f_meas
+)
+
+results <- test_preds %>%
+  class_metrics(
+    truth = truth,
+    estimate = estimate,
+    event_level = "second" 
+  )
+results
+
+##################-Question 3-#################
+library(reticulate)
+library(keras)
+library(tensorflow)
+py_require("tensorflow")
+
+set.seed(123)
+partitions <- claims_parsed %>%
+  initial_split(prop = 0.8)
+
+train_data <- training(partitions)
+test_data  <- testing(partitions)
+
+# extract text and labels
+train_text   <- train_data$text_clean
+train_labels <- ifelse(train_data$bclass == "Relevant claim content", 1, 0)
+
+test_text    <- test_data$text_clean
+test_labels  <- ifelse(test_data$bclass == "Relevant claim content", 1, 0)
+
+# create text preprocessing layer
+preprocess_layer <- layer_text_vectorization(
+  standardize = NULL,
+  split = 'whitespace',
+  ngrams = NULL,
+  max_tokens = NULL,
+  output_mode = 'tf_idf'
+)
+preprocess_layer %>% adapt(train_text)
+
+# define neural network
+model <- keras_model_sequential() %>%
+  preprocess_layer() %>%
+  layer_dropout(0.2) %>%
+  layer_dense(units = 25, activation = 'relu') %>%
+  layer_dropout(0.2) %>%
+  layer_dense(1, activation = 'sigmoid')  # binary output
+
+model %>% compile(
+  loss = 'binary_crossentropy',
+  optimizer = 'adam',
+  metrics = 'binary_accuracy'
+)
+
+# train the model
+history <- model %>%
+  fit(
+    x = train_text,
+    y = train_labels,
+    validation_split = 0.3,
+    epochs = 5,
+    batch_size = 32
+  )
+
+# predictions on test set
+pred_probs <- model %>% predict(test_text) %>% as.numeric()
+
+# convert probabilities to factor with levels 0 and 1
+pred_classes <- factor(ifelse(pred_probs > 0.5, 1, 0), levels = c(0,1))
+truth <- factor(test_labels, levels = c(0,1))
+
+# compute metrics using the same class_metrics function as PCA logistic regression
+class_metrics <- metric_set(
+  yardstick::accuracy,
+  yardstick::sensitivity,
+  yardstick::specificity,
+  yardstick::f_meas
+)
+
+nn_results <- tibble(
+  truth = truth,
+  estimate = pred_classes
+) %>%
+  class_metrics(
+    truth = truth,
+    estimate = estimate,
+    event_level = "second"
+  )
+
+nn_results
