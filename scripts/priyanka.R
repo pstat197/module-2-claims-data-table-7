5112bc5a4190f52fc2c3278cfc4783305c30cd5e
priyankaadurai <priyankadurai@ucsb.edu>
2025-11-17 22:39:59 -0800

    Fix preliminary question 1

--- scripts/priyanka.R
+++ scripts/priyanka.R
@@ -11,11 +11,62 @@ require(rvest)
 require(qdapRegex)
 require(stopwords)
 require(tokenizers)
-library(tidyr)
-library(dplyr)
 
 # function to parse html and clean text
 parse_fn <- function(.html){
+  read_html(.html) %>%
+    html_elements('p') %>%
+    html_text2() %>%
+    str_c(collapse = ' ') %>%
+    rm_url() %>%
+    rm_email() %>%
+    str_remove_all('\'') %>%
+    str_replace_all(paste(c('\n', 
+                            '[[:punct:]]', 
+                            'nbsp', 
+                            '[[:digit:]]', 
+                            '[[:symbol:]]'),
+                          collapse = '|'), ' ') %>%
+    str_replace_all("([a-z])([A-Z])", "\\1 \\2") %>%
+    tolower() %>%
+    str_replace_all("\\s+", " ")
+}
+
+# function to apply to claims data
+parse_data <- function(.df){
+  out <- .df %>%
+    filter(str_detect(text_tmp, '<!')) %>%
+    rowwise() %>%
+    mutate(text_clean = parse_fn(text_tmp)) %>%
+    unnest(text_clean) 
+  return(out)
+}
+
+nlp_fn <- function(parse_data.out){
+  out <- parse_data.out %>% 
+    unnest_tokens(output = token, 
+                  input = text_clean, 
+                  token = 'words',
+                  stopwords = str_remove_all(stop_words$word, 
+                                             '[[:punct:]]')) %>%
+    mutate(token.lem = lemmatize_words(token)) %>%
+    filter(str_length(token.lem) > 2) %>%
+    count(.id, bclass, token.lem, name = 'n') %>%
+    bind_tf_idf(term = token.lem, 
+                document = .id,
+                n = n) %>%
+    pivot_wider(id_cols = c('.id', 'bclass'),
+                names_from = 'token.lem',
+                values_from = 'tf_idf',
+                values_fill = 0)
+  return(out)
+}
+
+claims_parsed <- parse_data(claims_raw)
+claims_nlp <- nlp_fn(claims_parsed)
+
+# function to parse html and clean text
+parse_fn_header <- function(.html){
   html <- read_html(.html) 
   
   header <- html %>%
@@ -49,16 +100,18 @@ parse_fn <- function(.html){
 }
 
 # function to apply to claims data
-parse_data <- function(.df){
+parse_data_header <- function(.df){
   out <- .df %>%
     filter(str_detect(text_tmp, '<!')) %>%
     rowwise() %>%
-    mutate(text_clean = parse_fn(text_tmp)) %>%
-    unnest_wider(text_clean) 
+    mutate(text_clean = parse_fn_header(text_tmp)) %>%
+    unnest_wider(text_clean) %>%
+    mutate(text_clean = str_c(header, text_clean, sep = " ")) %>% 
+    select(-header)
   return(out)
 }
 
-nlp_fn <- function(parse_data.out){
+nlp_fn_header <- function(parse_data_header.out){
   out <- parse_data.out %>% 
     unnest_tokens(output = token, 
                   input = text_clean, 
@@ -78,10 +131,10 @@ nlp_fn <- function(parse_data.out){
   return(out)
 }
 
-claims_parsed <- parse_data(claims_raw)
-claims_nlp <- nlp_fn(claims_parsed)
+claims_parsed_header <- parse_data_header(claims_raw)
+claims_nlp_header <- nlp_fn(claims_parsed_header)
 
-######-Fit PCA Logistic Regression Model-##########
+######-Fit PCA Logistic Regression Model (no headers)-##########
 library(dplyr)
 library(rsample)
 library(yardstick)
@@ -138,7 +191,61 @@ results <- test_preds %>%
     estimate = estimate,
     event_level = "second" 
   )
+
+######-Fit PCA Logistic Regression Model (with headers)-##########
+claims_subset <- claims_nlp_header %>%
+  select(-.id) %>%
+  mutate(class = ifelse(bclass == "Relevant claim content", 1, 0)) %>%
+  select(-bclass)
+
+set.seed(101422)
+claims_split <- initial_split(claims_subset, prop = 0.8)
+train_data <- training(claims_split)
+View(train_data)
+test_data  <- testing(claims_split)
+
+X_train <- as.matrix(select(train_data, -class))
+X_test  <- as.matrix(select(test_data, -class))
+
+zero_var_cols <- apply(X_train, 2, function(col) var(col) == 0)
+X_train_filtered <- X_train[, !zero_var_cols]
+X_test_filtered  <- X_test[, !zero_var_cols]
+
+pca_res <- prcomp(X_train_filtered, center = TRUE, scale. = FALSE)
+cumvar <- cumsum(pca_res$sdev^2 / sum(pca_res$sdev^2))
+n_pc <- which(cumvar >= 0.9)[1]
+
+X_train_pca <- as.data.frame(pca_res$x[, 1:n_pc])
+X_test_pca  <- as.data.frame(predict(pca_res, newdata = X_test_filtered)[, 1:n_pc])
+
+X_train_pca$class <- train_data$class
+X_test_pca$class  <- test_data$class
+
+fit_pca <- glm(class ~ ., data = X_train_pca, family = "binomial")
+
+test_preds <- augment(fit_pca, newdata = X_test_pca, type.predict = "response") %>%
+  rename(pred_prob = .fitted) %>%
+  mutate(
+    estimate = factor(ifelse(pred_prob > 0.5, 1, 0), levels = c(0,1)),
+    truth    = factor(class, levels = c(0,1))
+  )
+
+class_metrics <- metric_set(
+  yardstick::accuracy,
+  yardstick::sensitivity,
+  yardstick::specificity,
+  yardstick::f_meas
+)
+
+results_header <- test_preds %>%
+  class_metrics(
+    truth = truth,
+    estimate = estimate,
+    event_level = "second" 
+  )
+
 results
+results_header
 
 ##################-Question 3-#################
 library(reticulate)
